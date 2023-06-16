df5e4b9df7bd742183eb121b686a4f595512efe3
minntoko <minntoko0822@gmail.com>
2023-06-16 22:38:35 +0900

    add: Object型について

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-var add = function (n1, n2, showResult, phrase) {
-    // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //   throw new Error('入力値が正しくありません');
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
+var person = {
+    name: "yota",
+    age: 30
 };
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result: ';
-var result = add(number1, number2, printResult, resultPhrase);
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,20 +1,10 @@
-const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
-  // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('入力値が正しくありません');
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+// const person: {
+//   name: string;
+//   age: number;
+// }
+const person = {
+  name: "yota",
+  age: 31
+};
 
-let number1: number;
-number1 = 5;
-const number2 = 2.8; // 完璧に型が推論される場合は型を明示する必要はない
-const printResult = true;
-let resultPhrase = 'Result: ';
-// resultPhrase = 0;
-
-const result = add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
