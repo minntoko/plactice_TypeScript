f3b85b76d5e45a3bf4711c3886d3e76c470c73d0
minntoko <minntoko0822@gmail.com>
2023-06-17 17:15:31 +0900

    add: tuple型

--- app.ts
+++ app.ts
@@ -2,14 +2,24 @@
 //   name: string;
 //   age: number;
 // }
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // tuple型
+} = {
   name: "yota",
   age: 31,
-  hobbies: ["Sports", "Cooking"]
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"]
 };
 
-let favoriteActivities: any[];
-favoriteActivities = ["Sports", 1];
+// person.role.push("admin"); //pushは許可されてしまう
+// person.role[1] = 10;
+// person.role = [0, "admin", "user"];
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
 
 console.log(person.name);
 
