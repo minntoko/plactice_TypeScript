43021f75d270a433153ce32b090cd5ead8cde71c
minntoko <minntoko0822@gmail.com>
2023-06-17 17:35:56 +0900

    add: enum型

--- app.ts
+++ app.ts
@@ -2,16 +2,33 @@
 //   name: string;
 //   age: number;
 // }
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; // tuple型
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // tuple型
+// } = {
+//   name: "yota",
+//   age: 31,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"]
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY = 100,
+  AUTHOR = "AUTHOR"
+}
+
+const person = {
   name: "yota",
   age: 31,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"]
+  role: Role.ADMIN
 };
 
 // person.role.push("admin"); //pushは許可されてしまう
@@ -23,6 +40,11 @@ favoriteActivities = ["Sports"];
 
 console.log(person.name);
 
-for(const hobby of person.hobbies) {
+for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("管理者ユーザー");
+  console.log(Role.ADMIN);
 }
