a4b80be954b86444766ab63e2b8b751202e4c99d
minntoko <minntoko0822@gmail.com>
2023-06-17 19:52:20 +0900

    add: function型

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ var add = function (n1, n2) {
 var printResult = function (num) {
     console.log("Result: " + num);
 };
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(3, 5));
 printResult(add(5, 12));
