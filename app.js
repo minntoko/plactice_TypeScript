0ca27b86787cf715927576b502463ef59376b6ab
minntoko <minntoko0822@gmail.com>
2023-06-17 16:44:21 +0900

    add: Arry型を追加

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//   name: string;
+//   age: number;
+// }
 var person = {
     name: "yota",
-    age: 30
+    age: 31,
+    hobbies: ["Sports", "Cooking"]
 };
-console.log(person);
+var favoriteActivities;
+favoriteActivities = ["Sports", 1];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -4,7 +4,15 @@
 // }
 const person = {
   name: "yota",
-  age: 31
+  age: 31,
+  hobbies: ["Sports", "Cooking"]
 };
 
+let favoriteActivities: any[];
+favoriteActivities = ["Sports", 1];
+
 console.log(person.name);
+
+for(const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
