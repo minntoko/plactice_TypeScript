41934f7966ea56fb97d738e83af699f3aac3e8dc
minntoko <minntoko0822@gmail.com>
2023-06-18 17:41:17 +0900

    add: Libの説明

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
-let age;
-age = 30;
-const userName = "Maximilian";
-console.log(userName);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log("clicked!");
+});
+const map = new Map();
--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
-let age: number;
-age = 30;
-const userName = "Maximilian";
-console.log(userName);
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => {
+  console.log("clicked!");
+});
+
+const map = new Map();
