e0155461e4db49566b5b219a9f47f3cc2ed13b21
minntoko <minntoko0822@gmail.com>
2023-06-19 21:23:34 +0900

    add: getter, setter

--- dist/app.js
+++ dist/app.js
@@ -23,12 +23,26 @@ class ITDepartment extends Department {
     }
 }
 class AccountingDepartment extends Department {
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('レポートが見つかりません。');
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("正しい値を設定してください");
+        }
+        this.addReport(value);
+    }
     constructor(id, reports) {
         super(id, "Accounting");
         this.reports = reports;
+        this.lastReport = this.reports[0];
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReports() {
         console.log(this.reports);
@@ -47,7 +61,9 @@ it.describe();
 it.printEmployeeInformaiton();
 console.log(it);
 const accounting = new AccountingDepartment("d2", []);
+accounting.mostRecentReport = "通期会計レポート";
 accounting.addReport("Something");
+console.log(accounting.mostRecentReport);
 accounting.printReports();
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
