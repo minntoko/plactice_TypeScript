11f1fad929fb81be442391d2aaa1b963d2af25f7
minntoko <minntoko0822@gmail.com>
2023-06-20 20:44:57 +0900

    add: クラスでのインターフェースの実装

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,14 @@
 "use strict";
-let user1;
-user1 = {
-    name: "Max",
-    age: 30,
+class Person {
+    constructor(n) {
+        this.age = 30;
+        this.name = n;
+    }
     greet(phrase) {
         console.log(phrase + " " + this.name);
     }
-};
+}
+let user1;
+user1 = new Person("Max");
 user1.greet("Hello I'm");
+console.log(user1);
