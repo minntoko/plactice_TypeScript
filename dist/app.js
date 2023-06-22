7acc5ddc35189519da20996c0d90d8a4b8d7709c
minntoko <minntoko0822@gmail.com>
2023-06-22 19:47:41 +0900

    add: Genericsに制約を追加する

--- dist/app.js
+++ dist/app.js
@@ -2,5 +2,5 @@
 function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
-const mergedObj = merge({ name: "Max" }, { age: 30 });
-mergedObj.age;
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj);
