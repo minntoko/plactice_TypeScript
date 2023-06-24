8587d513f6dcb1e6b80627a200b25b4a4ecbba94
minntoko <minntoko0822@gmail.com>
2023-06-24 19:52:36 +0900

    add: 最初のクラスデコレータ

--- dist/app.js
+++ dist/app.js
@@ -1,49 +1,22 @@
 "use strict";
-function merge(objA, objB) {
-    return Object.assign(objA, objB);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(constructor) {
+    console.log("ログ出力中...");
+    console.log(constructor);
 }
-const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
-console.log(mergedObj);
-function countAndDiscribe(element) {
-    let descriptionText = "値がありません。";
-    if (element.length > 0) {
-        descriptionText = "値は" + element.length + "個です。";
-    }
-    return [element, descriptionText];
-}
-console.log(countAndDiscribe(["Sports", "Cooking"]));
-function extractAndConvert(obj, key) {
-    return "Value: " + obj[key];
-}
-extractAndConvert({ name: "Max" }, "name");
-class DataStorage {
+let Person = class Person {
     constructor() {
-        this.data = [];
+        this.name = "Max";
+        console.log("Personオブジェクトを作成中...");
     }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
-        }
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.data];
-    }
-}
-const textStorage = new DataStorage();
-textStorage.addItem("Data1");
-textStorage.addItem("Data2");
-textStorage.removeItem("Data1");
-console.log(textStorage.getItems());
-const numberStorage = new DataStorage();
-function createCourseGoal(title, description, date) {
-    let courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal;
-}
-const names = ["Max", "Anna"];
+};
+Person = __decorate([
+    Logger
+], Person);
+const pers = new Person();
+console.log(pers);
