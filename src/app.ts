bf74c45ab7ec3c47e83b6713932fccb97dc3b070
minntoko <minntoko0822@gmail.com>
2023-06-27 18:54:53 +0900

    add: DOM要素の操作

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,34 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
   constructor() {
     this.templateElement = document.querySelector("#project-input")!;
     this.hostElement = document.querySelector("#app")!;
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input"
 
+    this.titleInputElement = this.element.querySelector("#title");
+    this.descriptionInputElement = this.element.querySelector("#description");
+    this.mandayInputElement = this.element.querySelector("#manday");
+
+    this.configure();
     this.attach();
   }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this))
+  }
+
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
