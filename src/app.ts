090e7d2bfd978e7eafcf2d69946bfc280c75b472
minntoko <minntoko0822@gmail.com>
2023-06-28 13:03:45 +0900

    add: プロジェクトの一覧表示

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,36 @@ function autobind(
   return adjDescriptor;
 }
 
+// ProjectListClass
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.querySelector("#project-list")!;
+    this.hostElement = document.querySelector("#app")!;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent = this.type === "active" ? "実行中プロジェクト" : "完了プロジェクト";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+// ProjectInputClass
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -128,3 +158,5 @@ class ProjectInput {
   }
 }
 const prjInput = new ProjectInput();
+const activPrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
