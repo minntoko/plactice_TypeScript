9f82a4f9bab8a244de96cbcfdd00b7b67cf22ecc
minntoko <minntoko0822@gmail.com>
2023-07-09 18:45:04 +0900

    add: Axios&住所の座標取得

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,42 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
+const GOOGLE_API_KEY = "";
+
+type GoogleGeometryResponse = {
+  results: {
+    geometry: {
+      location: {
+        lat: number,
+        lng: number
+      }
+    }
+  }[];
+  status: "OK" | "ZERO_RESULTS";
+}
+
+
 const searchAddressHandler = (event: Event) => {
   event.preventDefault();
-  const enteredAddress =  addressInput.value;
+  const enteredAddress = addressInput.value;
 
   // GoogleAPIに送信
+  axios.get<GoogleGeometryResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+    enteredAddress
+  )}&key=${GOOGLE_API_KEY}`
+  ).then(response => {
+    if(response.data.status != "OK") {
+      throw new Error("座標を取得できませんでした。");
+      
+    }
+    const coordinates = response.data.results[0].geometry.location;
+    console.log(coordinates);
+  }).catch(err => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler)
