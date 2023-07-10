743a8743152655060851605c47a07e6ac6deb3aa
minntoko <minntoko0822@gmail.com>
2023-07-10 19:55:23 +0900

    add: Propsの利用&Propsの型

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "TypeScriptコースの完了" }];
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <TodoList items={todos} />
+    </div>
+  );
+};
 
-export default App
+export default App;
