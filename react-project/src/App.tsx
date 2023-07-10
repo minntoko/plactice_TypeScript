c97e6dd7d9112c68f6f2f49252f3ac2a6c8a92d3
minntoko <minntoko0822@gmail.com>
2023-07-10 20:19:14 +0900

    add: refを使ったユーザー入力の取得

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "TypeScriptコースの完了" }];
   return (
     <div>
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
