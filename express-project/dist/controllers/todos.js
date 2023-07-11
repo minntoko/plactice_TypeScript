86d94ca3ed77f9618016fce817109a85d50046c2
minntoko <minntoko0822@gmail.com>
2023-07-11 20:47:39 +0900

    add: CRUD処理を追加

--- express-project/dist/controllers/todos.js
+++ express-project/dist/controllers/todos.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.deleteTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const TODOS = [];
 const createTodo = (req, res, next) => {
@@ -11,3 +11,29 @@ const createTodo = (req, res, next) => {
     res.status(201).json({ message: 'TODOを作成しました。', createdTodo: newTodo });
 };
 exports.createTodo = createTodo;
+const getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.getTodos = getTodos;
+const updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const text = req.body.text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("対象のToDoが見つかりませんでした。");
+    }
+    TODOS[todoIndex] = new todo_1.Todo(todoId, text);
+    res.json({ message: "更新しました。", updatedTodo: TODOS[todoIndex] });
+};
+exports.updateTodo = updateTodo;
+const deleteTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const text = req.body.text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("対象のToDoが見つかりませんでした。");
+    }
+    const deletedTarget = TODOS.splice(todoIndex, 1);
+    res.json({ message: "ToDoを削除しました。", deletedTodo: deletedTarget });
+};
+exports.deleteTodo = deleteTodo;
--- express-project/src/controllers/todos.ts
+++ express-project/src/controllers/todos.ts
@@ -5,12 +5,46 @@ import { Todo } from '../models/todo';
 const TODOS: Todo[] = [];
 
 export const createTodo: RequestHandler = (req, res, next) => {
-  const text = (req.body as {text: string}).text;
+  const text = (req.body as { text: string }).text;
   const newTodo = new Todo(Math.random().toString(), text);
 
   TODOS.push(newTodo);
   console.log("実行中");
-  
 
-  res.status(201).json({message: 'TODOを作成しました。', createdTodo: newTodo});
+
+  res.status(201).json({ message: 'TODOを作成しました。', createdTodo: newTodo });
+}
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+}
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const text = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("対象のToDoが見つかりませんでした。");
+  }
+
+  TODOS[todoIndex] = new Todo(todoId, text);
+
+  res.json({ message: "更新しました。", updatedTodo: TODOS[todoIndex] });
+}
+
+export const deleteTodo: RequestHandler = (req, res, next) => {
+  const todoId = req.params.id;
+  const text = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("対象のToDoが見つかりませんでした。");
+  }
+
+  const deletedTarget = TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "ToDoを削除しました。", deletedTodo: deletedTarget });
 }
